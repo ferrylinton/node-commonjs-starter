a7bfb0ec911dd04078f8252ac7ca06e611b314cb
Ferry L.H <ferrylinton@gmail.com>
2023-11-29 08:19:58 +0700

    update config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,27 @@
 module.exports = {
- env: {
-  commonjs: true,
-  node: true,
-  es6: true,
-  jest: true,
- },
- extends: ['plugin:prettier/recommended', 'prettier', 'eslint:recommended'],
- overrides: [
-  {
-   files: ['.eslintrc.{js,cjs}', '**/*.test.js'],
-   parserOptions: {
-    sourceType: 'script',
-   },
-   plugins: ['jest'],
-  },
- ],
- parserOptions: {
-  ecmaVersion: 'latest',
- },
- rules: {
-  'no-var': 'error',
-  semi: 'error',
-  'no-multi-spaces': 'error',
-  'space-in-parens': 'error',
-  'no-multiple-empty-lines': 'error',
-  'prefer-const': 'error',
- },
+	env: {
+		commonjs: true,
+		node: true,
+		es6: true,
+		jest: true,
+	},
+	extends: ['plugin:prettier/recommended', 'prettier', 'eslint:recommended'],
+	overrides: [
+		{
+			files: ['.eslintrc.{js,cjs}', '**/*.test.js'],
+			parserOptions: {
+				sourceType: 'script',
+			},
+			plugins: ['jest'],
+		},
+	],
+	parserOptions: {
+		ecmaVersion: 'latest',
+	},
+	rules: {
+		'prettier/prettier': 'warn',
+		'no-unused-vars': 'warn',
+		'no-var': 'warn',
+		'prefer-const': 'warn',
+	},
 };
